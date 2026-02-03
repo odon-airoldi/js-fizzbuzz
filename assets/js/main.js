5e0d46e75841c4a2c8273ab410c4d2e0dbb90e67
odon-airoldi <odon.airoldi@gmail.com>
2026-02-03 16:40:18 +0100

    solution with relational diversity operator

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,3 @@
-
 /*
 Scrivi un programma che stampi i numeri da 1 a 100,
 - per i multipli di 3 stampi Fizz al posto del numero
@@ -6,19 +5,52 @@ Scrivi un programma che stampi i numeri da 1 a 100,
 - per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz
 */
 
+// Soluzione cambiando l'ordine delle condizioni
+
 for ( let i = 1; i <= 100; i++ ) {
 
+    // se l'indice è divisibile sia per 3 che per 5
     if (i % 3 == 0 && i % 5 == 0) {
 
         console.log('FizzBuzz')
 
+    // altrimenti se è divisibile per 3
+    } else if (i % 3 == 0) {
+
+        console.log('Fizz')
+
+    // altrimenti se è divisibile per 5
     } else if (i % 5 == 0) {
+
+        console.log('Buzz');
+
+    // altrimenti
+    } else {
+
+        console.log(i)
+
+    }
+
+}
+
+// Soluzione con operatore relazionale di diversità, l'ordine delle condizioni non influisce sul risultato
+
+for ( let i = 1; i <= 100; i++ ) {
+
+    // se l'indice è divisibile per 3 e se l'indice non è divisibile per 5
+    if (i % 3 == 0 && i % 5 != 0 ) {
+
+        console.log('Fizz')
+
+    // se l'indice è divisibile per 5 e se l'indice non è divisibile per 3
+    } else if (i % 5 == 0 && i % 3 != 0) {
 
         console.log('Buzz')
 
-    } else if (i % 3 == 0) {
+    // se l'indice è divisibile sia per 3 che per 5
+    } else if (i % 3 == 0 && i % 5 == 0) {
 
-        console.log('Fizz');
+        console.log('FizzBuzz');
 
     } else {
 
