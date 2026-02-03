16fb63505480fb53776f52f11d65cf37a814fda6
odon-airoldi <odon.airoldi@gmail.com>
2026-02-03 15:03:25 +0100

    start statement for

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+
+/*
+Scrivi un programma che stampi i numeri da 1 a 100,
+- per i multipli di 3 stampi Fizz al posto del numero
+- per i multipli di 5 stampi Buzz
+- per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz
+*/
+
+for ( let i = 1; i <= 100; i++ ) {
+
+    console.log(i)
+
+}
