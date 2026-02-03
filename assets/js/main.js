dfd1a4f636a6d7c903df795990c4fac542b34dd3
odon-airoldi <odon.airoldi@gmail.com>
2026-02-03 15:12:29 +0100

    conditions for multiples

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,6 +8,22 @@ Scrivi un programma che stampi i numeri da 1 a 100,
 
 for ( let i = 1; i <= 100; i++ ) {
 
-    console.log(i)
+    if (i % 3 == 0 && i % 5 == 0) {
+
+        console.log('FizzBuzz')
+
+    } else if (i % 5 == 0) {
+
+        console.log('Buzz')
+
+    } else if (i % 3 == 0) {
+
+        console.log('Fizz');
+
+    } else {
+
+        console.log(i)
+
+    }
 
 }
